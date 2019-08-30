c06629adf383adb2ae5db3fa1d87d47193dc77e3
lcworld <lcworld>
2019-08-31 00:04:40 +0200

    Settings screen created and working

--- octoprint_mmu2single/__init__.py
+++ octoprint_mmu2single/__init__.py
@@ -1,29 +1,25 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
-class Mmu2singlePlugin(octoprint.plugin.SettingsPlugin,
+class MMU2SinglePlugin(octoprint.plugin.StartupPlugin,
+					   octoprint.plugin.SettingsPlugin,
                        octoprint.plugin.AssetPlugin,
                        octoprint.plugin.TemplatePlugin):
+	##~~ log settings on startup
+	def on_after_startup(self):
+		self._logger.info("nbFilaments: %d" % self._settings.get_int(["nbFilaments"]))
+		self._logger.info("filamentToUse: %d" % self._settings.get_int(["filamentToUse"]))
 
 	##~~ SettingsPlugin mixin
-
 	def get_settings_defaults(self):
 		return dict(
-			# put your plugin's default settings here
+			nbFilaments=5,
+			filamentToUse=0
 		)
 
 	##~~ AssetPlugin mixin
-
 	def get_assets(self):
 		# Define your plugin's asset files to automatically include in the
 		# core UI here.
@@ -34,14 +30,13 @@ class Mmu2singlePlugin(octoprint.plugin.SettingsPlugin,
 		)
 
 	##~~ Softwareupdate hook
-
 	def get_update_information(self):
 		# Define the configuration for your plugin to use with the Software Update
 		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
 		# for details.
 		return dict(
 			mmu2single=dict(
-				displayName="Mmu2single Plugin",
+				displayName="MMU2Single Plugin",
 				displayVersion=self._plugin_version,
 
 				# version check: github repository
@@ -55,15 +50,22 @@ class Mmu2singlePlugin(octoprint.plugin.SettingsPlugin,
 			)
 		)
 
+	##~~ Define templates
+	def get_template_configs(self):
+		return [
+			# dict(type="navbar", custom_bindings=False),
+			dict(type="settings", custom_bindings=True)
+		]
+
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Mmu2single Plugin"
+__plugin_name__ = "MMU2Single Plugin"
 
 def __plugin_load__():
 	global __plugin_implementation__
-	__plugin_implementation__ = Mmu2singlePlugin()
+	__plugin_implementation__ = MMU2SinglePlugin()
 
 	global __plugin_hooks__
 	__plugin_hooks__ = {
--- octoprint_mmu2single/static/js/mmu2single.js
+++ octoprint_mmu2single/static/js/mmu2single.js
@@ -8,11 +8,53 @@ $(function() {
     function Mmu2singleViewModel(parameters) {
         var self = this;
 
-        // assign the injected parameters, e.g.:
-        // self.loginStateViewModel = parameters[0];
-        // self.settingsViewModel = parameters[1];
+        self.settings = parameters[0];
 
-        // TODO: Implement your plugin's view model here.
+        self.nbFilaments = ko.observable();
+        // push form modifications to settings
+        self.nbFilaments.subscribe(function(newValue) {
+            self.settings.settings.plugins.mmu2single.nbFilaments(newValue);
+            self.refreshNBFilaments();              
+        });
+
+        self.filamentsList = ko.observableArray();
+
+        self.filamentToUse = ko.observable();
+        // push form modifications to settings
+        self.filamentToUse.subscribe(function(newValue) {
+            if (newValue !== undefined) {
+                self.settings.settings.plugins.mmu2single.filamentToUse(newValue);
+            }
+        });
+
+        // refresh list of filaments
+        self.refreshNBFilaments = function () {
+            // get nb of filament
+            var f = self.settings.settings.plugins.mmu2single.nbFilaments();
+
+            // get current filament
+            var s = self.settings.settings.plugins.mmu2single.filamentToUse();
+            // if not in nb of filament, set filament to use as first one (0 based)
+            if (0 > s || s > (f-1) ) {
+                s=0;
+            }
+            
+            // clean list and reinsert all new filaments
+            // text of operation defined in template
+            self.filamentsList.removeAll();            
+            for (var i=0 ; i < f ; i++) {
+                self.filamentsList.push(i);
+            }
+
+            // set current filament back
+            self.filamentToUse(s);
+        };
+
+        self.onBeforeBinding = function() {
+            //console.log(self.settings.settings.plugins.mmu2single.nbFilaments());
+            self.nbFilaments(self.settings.settings.plugins.mmu2single.nbFilaments());
+            self.refreshNBFilaments();
+        }
     }
 
     /* view model class, parameters for constructor, container to bind to
@@ -22,8 +64,8 @@ $(function() {
     OCTOPRINT_VIEWMODELS.push({
         construct: Mmu2singleViewModel,
         // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
-        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        dependencies: [ "settingsViewModel" ],
         // Elements to bind to, e.g. #settings_plugin_mmu2single, #tab_plugin_mmu2single, ...
-        elements: [ /* ... */ ]
+        elements: [ "#settings_plugin_mmu2single" ]
     });
 });
